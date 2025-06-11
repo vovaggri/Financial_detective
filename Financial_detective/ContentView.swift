ec676d55da79a34375424ab81d591e1765ac3f64
vovaggri <vladimirgri0405@gmail.com>
2025-06-11 20:49:39 +0300

    first commit

---
+++ Financial_detective/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Financial_detective
+//
+//  Created by Vladimir Grigoryev on 11.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Financial_detective/Financial_detectiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Financial_detectiveApp.swift
+//  Financial_detective
+//
+//  Created by Vladimir Grigoryev on 11.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct Financial_detectiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
