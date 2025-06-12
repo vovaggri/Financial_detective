dbe292cb72c1561966446f876f7607e428a67563
vovaggri <vladimirgri0405@gmail.com>
2025-06-12 19:18:39 +0300

    Setted File Cache of transactions

--- Financial_detective/FileCache/TransactionsFileCache.swift
+++ Financial_detective/FileCache/TransactionsFileCache.swift
@@ -1,10 +1,11 @@
 import Foundation
 
-enum TransactionFileCacheError: Error {
+enum TransactionsFileCacheError: Error {
     case documentsDirectoryNotFound
+    case invalidJSONStructure
 }
 
-final class TransactionFileCache {
+final class TransactionsFileCache {
     private let fileURL: URL
     private var transactions: [Transaction] = []
     
@@ -12,8 +13,59 @@ final class TransactionFileCache {
         transactions
     }
     
-    init(fileName: String) throws {
+    init(fileName: String = "transactions.json") throws {
         let fileManager = FileManager.default
-        let urls = fileManager.
+        let urls = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+        guard let docs = urls.first else {
+            throw TransactionsFileCacheError.documentsDirectoryNotFound
+        }
+        self.fileURL = docs.appendingPathComponent(fileName)
+    }
+    
+    func load() throws {
+        guard FileManager.default.fileExists(atPath: fileURL.path) else {
+            transactions = []
+            return
+        }
+        
+        let data = try Data(contentsOf: fileURL)
+        let jsonAny = try JSONSerialization.jsonObject(with: data, options: [])
+        
+        guard let array = jsonAny as? [Any] else {
+            throw TransactionsFileCacheError.invalidJSONStructure
+        }
+        
+        var parsed = [Transaction]()
+        for element in array {
+            if let transaction = Transaction.parse(jsonObject: element) {
+                parsed.append(transaction)
+            }
+        }
+        
+        var unique = [Int: Transaction]()
+        for transaction in parsed {
+            if unique[transaction.id] == nil {
+                unique[transaction.id] = transaction
+            }
+        }
+        transactions = Array(unique.values)
+    }
+    
+    func save() throws {
+        let jsonArray = transactions.map { $0.jsonObject }
+        
+        let data = try JSONSerialization.data(withJSONObject: jsonArray, options: [.prettyPrinted])
+        try data.write(to: fileURL, options: [.atomic])
+    }
+    
+    func add(_ transaction: Transaction) {
+        guard !transactions.contains(where: { $0.id == transaction.id }) else {
+            return
+        }
+        transactions.append(transaction)
+    }
+    
+    func remove(id: Int) {
+        transactions.removeAll { $0.id == id }
     }
 }
