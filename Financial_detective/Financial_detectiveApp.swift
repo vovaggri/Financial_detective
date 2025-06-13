ea41162424198103da7c1b35736b665f90a7f921
vovaggri <vladimirgri0405@gmail.com>
2025-06-13 12:38:41 +0300

    Added test screen

--- Financial_detective/Financial_detectiveApp.swift
+++ Financial_detective/Financial_detectiveApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Financial_detectiveApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ServicesTestView()
         }
     }
 }
